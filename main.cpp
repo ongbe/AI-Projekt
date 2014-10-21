e51ff3c26abcb97aa7424b07cab7f7d4187b0f27
Niklas <mailaInte@a.b>
2014-10-21 14:03:53 +0200

    lite nytt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "HMM.hpp"
+#include <vector>
 
 int main()
 {
-	std::cout << "hello world!\n";
+    int a[] = {1,2,3,4,5,1,2,3,4,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,1,2,3,1,2,2,2,3,4,2,3,4,2,3,3,1,0};
+	std::vector<int>in;
+	for(int i=0;i<90;++i)
+		in.push_back(a[i]);
+
+	HMM test(6,6); //create HMM obj
+	test.reset(); //random start guess
+	test.BaumWelch(in); //BaumWelch
+
+	std::cerr <<"Converged: "<< test.isConverged() << std::endl;
+
+    std::vector<int> ut = test.Viterbi(in);
+    for(int i=0; i<(int)ut.size();++i)
+    {
+        std::cerr << ut[i] << " ";
+    }
+    std::cerr << std::endl;
+
+
 	return 0;
 }
