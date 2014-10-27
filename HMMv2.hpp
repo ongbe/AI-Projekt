0ef3c3c1d7b224d286a83f5d03b6b2b35a80af42
Niklas <mailaInte@a.b>
2014-10-27 15:35:45 +0100

    lite lite till

--- HMMv2.hpp
+++ HMMv2.hpp
@@ -44,42 +44,42 @@ public:
                 A[i][j] = 0;
 
         // Bigram
-        for(int i=0; i<input.size()-1; i++)
+        for(int i=input.size()-2; i>=0; i--)
         {
-            A[input[i]][input[i+1]] += 1;
+            A[input[i+1]][input[i]] += 1;
             p[input[i]] += 1;
         }
-        for(int i=0; i<input.size()-1; i++)
+        for(int i=input.size()-2; i>=0; i--)
         {
-            A[input[i]][input[i+1]] = A[input[i]][input[i+1]]/p[input[i]];
+            A[input[i+1]][input[i]] = A[input[i+1]][input[i]]/p[input[i+1]];
         }
     }
 
     /**Generate sequence*/
-    std::vector<int> Generate(int startIndex, int length)
+    std::vector<int> Generate(int stopIndex, int length)
     {
         std::vector<int> seq(length);
-        int index = startIndex;
-        seq[0] = index;
-        for(int i=1;i<length;++i)
+        int index = stopIndex;
+        seq[length-1] = index;
+        for(int i=length-2;i>=0;--i)
         {
             int tempIndex = 0;
             double maximum = 0;
-        for(int j=0;j<N;++j)
-        {
-            for(int k=0; k<N; k++)
+            for(int j=0;j<N;++j)
             {
-                double temp = A[index][j]*A[j][k];
-                //std::cerr << temp << std::endl;
-                if(temp>maximum)
+                for(int k=0; k<N; k++)
                 {
-                    tempIndex = j;
-                    maximum = temp;
+                    double temp = A[index][j]*A[j][k];
+                    //std::cerr << temp << std::endl;
+                    if(temp>maximum)
+                    {
+                        tempIndex = j;
+                        maximum = temp;
+                    }
                 }
             }
-        }
-        index = tempIndex;
-        seq[i] = index;
+            index = tempIndex;
+            seq[i] = index;
         }
         return seq;
     }
