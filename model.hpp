4122663cba116b45d96f434847a7cffc893838d0
hannahx <milger@kth.se>
2014-10-28 15:17:21 +0100

    nåt som funkar - model

--- model.hpp
+++ model.hpp
@@ -26,7 +26,7 @@ class model
 private:
     int N;// = 9; States
     int M;// = 9; Observations
-
+    double** A;
     int* p; //number of times each word occurs
 
 public:
@@ -36,7 +36,7 @@ public:
         N=n;
         M=m;
         std::cerr << "constructor called size: " << N << "x" << M << std::endl;
-
+        A = initialize(M,N);
         p = (int*)calloc(N,sizeof(int));
         for(int i=0;i<N;++i)
             p[i] = 1;
@@ -81,8 +81,52 @@ public:
         seq[length-1] = index;
         for(int i=length-2;i>=0;--i)
         {
+            int tempIndex = 0;
+            double maximum = 0;
+            for(int j=0;j<N;++j)
+            {
+                //bigram
+                /*
+                if(A[index][j]>maximum)
+                {
+                    tempIndex = j;
+                    maximum = A[index][j]*val[j];
+                }*/
 
 
+                //trigram
+
+                for(int k=0; k<N; k++)
+                {
+                    double temp = A[index][j]*A[j][k]*val[j]*val[k];
+                    //std::cerr << temp << std::endl;
+                    if(temp>maximum)
+                    {
+                        tempIndex = j;
+                        maximum = temp;
+                    }
+                }
+
+                //quadgram
+                /*
+                std::cout << "ord: " << i << std::endl;
+                for(int k=0; k<N; k++)
+                {
+                    for(int l=0;l<N;++l)
+                    {§1§§§
+                        double temp = A[index][j]*A[j][k]*A[k][l]*val[j]*val[k]*val[l];
+                        if(temp>maximum)
+                        {
+                            tempIndex = j;
+                            maximum = temp;
+                        }
+                    }
+                }*/
+
+
+            }
+            index = tempIndex;
+            val[index]*=0.5;
             seq[i] = index;
         }
         return seq;
