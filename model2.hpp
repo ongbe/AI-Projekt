eaf8fee908d44196d43d57aa76bd9aeaa62b91ca
AndersSivert <AndersSivert@users.noreply.github.com>
2014-10-28 19:21:52 +0100

    meh

--- model2.hpp
+++ model2.hpp
@@ -6,8 +6,9 @@
 #include <sstream>
 #include <limits>
 #include <fstream>
+#include <unordered_map>
 
-/*struct wordObj
+struct wordObj
 {
     int word;
     int p1;
@@ -19,15 +20,17 @@
         p1 = plus1;
         p2 = plus2;
     }
-};*/
+};
 
 class model
 {
 private:
     int N;// = 9; States
     int M;// = 9; Observations
-
-    int* p; //number of times each word occurs
+    //double** A;
+    //int* p; //number of times each word occurs
+    std::unordered_map<int64_t, double> bigrams;
+    std::unordered_map<int64_t, double> trigrams;
 
 public:
     /**Contructor - generate matrices*/
@@ -36,10 +39,10 @@ public:
         N=n;
         M=m;
         std::cerr << "constructor called size: " << N << "x" << M << std::endl;
-
+        /*A = initialize(M,N);
         p = (int*)calloc(N,sizeof(int));
         for(int i=0;i<N;++i)
-            p[i] = 1;
+            p[i] = 1;*/
     }
 
     /**Creates a nxm matrix*/
@@ -57,33 +60,88 @@ public:
     */
     void learn(std::vector<int> input)
     {
-        // Bigram
-        for(int i=input.size()-2; i>=0; i--)
+        for (int i = input.size()-1; i > 1 ; i--)
         {
-            A[input[i+1]][input[i]] += 1;
-            p[input[i]] += 1;
-        }
-        for(int i=input.size()-2; i>=0; i--)
-        {
-            A[input[i+1]][input[i]] = A[input[i+1]][input[i]]/p[input[i]];
+            int tempBi[2] = {input[i],input[i-1]}, tempTri[3] = {input[i],input[i-1],input[i-2]};
+            int64_t t1 = (int64_t)tempBi, t2 = (int64_t)tempTri;
+            bigrams[t1] = (double)bigrams[t1]+1.; //If not previously existing (new bigram), automatically creates a node with value 0+1=1. Else, add 1 to value (count).
+            trigrams[t2] = (double)trigrams[t2]+1.;
         }
     }
 
     /**Generate sequence*/
-    std::vector<int> Generate(int stopIndex, int length)
+    /*std::vector<int> Generate(int lastWrd, int length)
     {
-        double val[N];
-        for(int i=0;i<N;++i)
-            val[i] = 1.;
-
-        std::vector<int> seq(length);
-        int index = stopIndex;
-        seq[length-1] = index;
-        for(int i=length-2;i>=0;--i)
+        std::cout << "Generation" << std::endl;
+        std::vector<int> out;
+        int bestWrd;
+        double maxFreq = 0, tempVal1, tempVal2;
+        for(int i = 0; i < N; i++)
         {
-            seq[i] = index;
+            for(int j = 0; j < N; j++)
+            {
+                //Count occurrences
+                int tempTri[3] = {lastWrd, i, j}, tempBi[2] = {i,j};
+                if (trigrams.find(tempTri) == trigrams.end()) tempVal1 = 1;
+                else tempVal1 = trigrams[tempTri]+1;
+                if (bigrams.find(tempBi) == bigrams.end()) tempVal2 = 1;
+                else tempVal2 = bigrams[tempBi]+1;
+
+                //Check if max
+                if (tempVal1/tempVal2 > maxFreq)
+                {
+                    bestWrd = i;
+                    maxFreq = tempVal1/tempVal2;
+                }
+            }
         }
-        return seq;
+        //Best word found! Generate next word
+        if (length >= 1)
+            out = GenerateReq(bestWrd, length-1);
+        out.push_back(lastWrd);
+        return out;
+    }*/
+    std::vector<int> Generate(int lastWrd, int length)
+    {
+        std::cout << "Generation: " << lastWrd << std::endl;
+        std::vector<int> out;
+
+        //Break condition
+        if (length == 1)
+        {
+            out.push_back(lastWrd);
+            return out;
+        }
+
+        //Consider
+        int bestWrd;
+        double maxFreq = 0, tempVal1, tempVal2;
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                //Count occurrences
+                int tempTri[3] = {lastWrd, i, j}, tempBi[2] = {lastWrd,i};
+                int64_t t1 = (int64_t)tempBi, t2 = (int64_t)tempTri;
+                if (trigrams.find(t2) == trigrams.end()) tempVal1 = 1.;
+                else tempVal1 = (trigrams[t2]+1.);
+                if (bigrams.find(t1) == bigrams.end()) tempVal2 = 1.;
+                else tempVal2 = (bigrams[t1]+1.);
+
+                //Check if max
+                //std::cout << tempVal1 << "\t" << tempVal2 << std::endl;
+                //std::cout << tempVal1/tempVal2 << std::endl;
+                if (tempVal1/tempVal2 > maxFreq)
+                {
+                    bestWrd = i;
+                    maxFreq = tempVal1/tempVal2;
+                }
+            }
+        }
+        //Best word found! Generate next word
+        out = Generate(bestWrd, length-1);
+        out.push_back(lastWrd);
+        return out;
     }
 
     void print (double ** AA)
--- testset.cpp
+++ testset.cpp
@@ -1,72 +1,13 @@
-#include <iostream>
-#include <unordered_set>
-#include <fstream>
-#include <sstream>
-//#include <string>
-#include <vector>
+#include <unordered_map>
+#include<iostream>
 
-std::unordered_set<std::string> wordset;
-std::vector<std::string> words;
-std::vector<int> index;
-//std::vector< std::vector<int> > sequences;
-int maxIndex = -1;
+std::unordered_map<int, int> bigrams;
 
-bool inVector(std::string in);
-int indexInVector(std::string in);
-std::string getWord(int i);
-
-
-bool inVector(std::string in)
+int main()
 {
-    if(wordset.find(in) != wordset.end()) return true;
-    else return false;
-}
-
-int main ()
-{
-    std::string line;
-    //std::ifstream myfile ("poems.txt");
-    std::ifstream myfile("ShakespeareSonnets.txt");
-
-    std::vector<int>sequence;
-
-    if (myfile.is_open())
-    {
-        /* Skip copyright notices etcetera */
-        //for(int i = 0; i < 288; ++i) getline(myfile,line);
-        /* Actual used code*/
-        while ( getline (myfile,line) && line != "End of The Project Gutenberg Etext of Shakespeare's Sonnets")
-        {
-            if (line.length() >= 10)
-            {
-                std::cout << line << std::endl;
-                //spara ord + Baumwelch + add
-                //std::vector<int>sequence;
-                std::istringstream iss;
-                iss.str(line);
-                while (!iss.eof())
-                {
-                    std::string temp;
-                    iss >> temp;
-                    //std::cerr << temp << std::endl;
-                    if(!inVector(temp))
-                    {
-                        maxIndex++;
-                        wordset.insert(temp);
-                        words.push_back(temp);
-                        index.push_back(maxIndex);
-                        sequence.push_back(maxIndex);
-                    }
-                    /*else
-                    {
-                        sequence.push_back(indexInVector(temp));
-                    }*/
-                }
-                //sequences.push_back(sequence);
-            }
-        }
-        myfile.close();
-    }
-    else std::cout << "Unable to open file";
-    std::cout << "Reading done" << std::endl;
+    int tempBi[2] = {2, 5};
+    int t = (int)tempBi;
+    std::cout << bigrams[t] << std::endl;
+    bigrams[t] = bigrams[t]+1;
+    std::cout << bigrams[t] << std::endl;
 }
